15dd8fa8a96f818cf71207795395e28ddd5ac6be
Duy Nguyen <duy_nobita@icloud.com>
2019-07-19 15:40:22 -0400

    Created Repository Project3

---
+++ client/build/precache-manifest.c6f6c457206f9eb64924a64a6ce43fa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee65ec65aef173f38f5a94318d81e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a519c2f577fbd1387a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "c2a519c2f577fbd1387a",
+    "url": "/static/js/main.adfade41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
